949c60020ede4228962e63a3d5272dd19d1a5539
Ajesh <ajesh.avirah@cse.christuniversity.in>
2019-05-23 14:39:00 +0530

    final Laravel-Blog-site : Commit

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,7 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 <!-- 6.1: Create the boostrap starter NavBar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="/"><h6><i class="fa fa-2x fa-feather-alt"></i> {{config('app.name', 'LsApp')}}<h6></a>
+    <a class="navbar-brand" href="{{ url('/')}}"><h6><i class="fa fa-2x fa-feather-alt"></i> {{config('app.name', 'LsApp')}}<h6></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
